160a50f8c9a5b275f898d041745ff2719a8ce397
martinlindelof <martin.lindelof@gmail.com>
2013-02-24 16:20:49 +0100

    added ofxTraerPhysics header file, more to ofx addon standard.

---
+++ src/ofxTraerPhysics.h
@@ -0,0 +1,27 @@
+/*
+
+88                                         88
+88                                         88
+88                                         88
+88,dPPYba,   ,adPPYba, ,adPPYYba,  ,adPPYb,88  ,adPPYba, 8b,dPPYba,
+88P'    "8a a8P_____88 ""     `Y8 a8"    `Y88 a8P_____88 88P'   "Y8
+88       88 8PP""""""" ,adPPPPP88 8b       88 8PP""""""" 88
+88       88 "8b,   ,aa 88,    ,88 "8a,   ,d88 "8b,   ,aa 88
+88       88  `"Ybbd8"' `"8bbdP"Y8  `"8bbdP"Y8  `"Ybbd8"' 88
+
+  		ofxTraerPhysics.h
+  		@author Martin Lindelof
+  		@description to make it simpler for folks not used to Traer.
+  		includes all headers.
+*/
+
+#pragma once
+
+#include "Attraction.h"
+#include "EulerIntegrator.h"
+#include "Force.h"
+#include "ModifiedEulerIntegrator.h"
+#include "Particle.h"
+#include "ParticleSystem.h"
+#include "RungeKuttaIntegrator.h"
+#include "Spring.h"
